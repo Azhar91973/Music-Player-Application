87734f0f9ef4d273f56aaef215c096e778b452d1
Azhar91973 <105579274+Azhar91973@users.noreply.github.com>
2023-07-08 20:52:06 +0530

    Add files via upload

---
+++ Music Player/code/MusicPlayer.py
@@ -0,0 +1,30 @@
+# Importing the pygame in build library
+import pygame
+
+# Creating the playlist
+playlist=[]
+pygame.mixer.init()
+
+
+#  Function for Adding the Song into The Playlist
+def add_song(path):
+        playlist.append(path)
+    
+# Function For Playing the Song 
+def play():
+     for song in playlist:
+         pygame.mixer.music.load(song)
+         pygame.mixer.music.play()
+         while pygame.mixer.music.get_busy():
+             continue        
+        
+#  Adding the Song's into playlist
+ 
+add_song("C:\\Music\\song.mp3")   
+add_song("C:\\Music\\Zehra.mp3")  
+add_song("C:\\Music\\Randall.mp3")  
+
+# Playing the song 
+play()
+         
+ 
